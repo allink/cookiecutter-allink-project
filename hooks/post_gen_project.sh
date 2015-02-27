57c9a3a6ec9eda52542204c59bb4241c9f16499b
leandrafinger <leandra.finger@gmail.com>
2015-02-27 11:22:49 +0100

    fixed cookiecutter project

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/env bash
 
 git init
+unzip {{cookiecutter.project_name}}/static/lib.zip -d {{cookiecutter.project_name}}/static
+rm {{cookiecutter.project_name}}/static/lib.zip
 git add --all
 git commit -m "initial commit"
 git checkout -b develop
