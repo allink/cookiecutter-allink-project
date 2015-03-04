60e744f4edf92c93477bec4fb4eb4c2bceb2107a
leandrafinger <leandra.finger@gmail.com>
2015-03-04 07:57:05 +0100

    signup tracking improved

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/static/javascript/main.js
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/static/javascript/main.js
@@ -5,7 +5,7 @@ $(function() {
         $.post($this.attr('action'), $this.serialize(), function(data) {
             if (data == 'ok'){
                 $this.parents('.mailchimp-result').html( 'Vielen Dank für Ihre Anmeldung.' );
-                _gaq.push(['_trackEvent', 'Newsletter anmeldung']);
+                _gaq.push(['_trackEvent', 'Newsletter', 'Sign up']);
             }
             else {
                 $this.parents('.mailchimp-result').html(data);
