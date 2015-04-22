6508ce3478e8ed7cfe47322f517eed09b03413db
leandrafinger <leandra.finger@gmail.com>
2015-04-22 11:57:12 +0200

    static folder problems

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/settings/development.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/settings/development.py
@@ -1,5 +1,7 @@
 from .default import *  # noqa
 
+import os
+
 # ===================
 # = Global Settings =
 # ===================
@@ -13,3 +15,9 @@ TEMPLATE_DEBUG = DEBUG
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FILE_STORAGE = 'allink_essentials.storage.ascii_file_system_storage.ASCIIFileSystemStorage'
+
+# ================
+# = File serving =
+# ================
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'compiled_static')
